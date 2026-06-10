1dea06b1f966f7cca2d6c356043910d7986de4d2
Aashifa <aashifaabu90@gmail.com>
2026-06-10 19:58:48 +0530

    Update task4_object_tracking.py

--- task4_object_tracking.py
+++ task4_object_tracking.py
@@ -1,43 +1,9 @@
 from ultralytics import YOLO
-import cv2
 
-# Load model
 model = YOLO("yolov8n.pt")
 
-cap = cv2.VideoCapture(0)
-
-object_id = 0  # fake simple tracking ID
-
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
-
-    results = model(frame)
-
-    for r in results:
-        boxes = r.boxes
-
-        for box in boxes:
-            x1, y1, x2, y2 = box.xyxy[0]
-            conf = box.conf[0]
-            cls = int(box.cls[0])
-
-            # Fake tracking ID (simple version)
-            object_id += 1
-
-            label = f"{model.names[cls]} ID:{object_id} {conf:.2f}"
-
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0,255,0), 2)
-            cv2.putText(frame, label, (x1, y1-10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 2)
-
-    cv2.imshow("Object Detection & Tracking", frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+model.track(
+    source=0,              # webcam
+    show=True,             # display output
+    tracker="bytetrack.yaml"
+)
